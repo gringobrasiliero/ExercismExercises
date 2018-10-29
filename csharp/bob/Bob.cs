b173c40808168502bd8b0455d4c3821a6f5e421a
Nolan Byrnes <nolanehs@gmail.com>
2018-10-29 10:39:26 -0400

    Bob Coding Challenge in C#

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,45 @@
+using System;
+
+public static class Bob
+{
+    public static string Response(string statement)
+    {
+        bool isQuestion = false;
+        bool allCaps = true;
+        bool noAlphaNum = true;
+        bool capExists = false;
+        bool questionMarkFound = false;
+
+        for (int i = 0; i < statement.Length; i++){
+          char c = statement[i];
+          if (char.IsLetterOrDigit(c)){
+            noAlphaNum = false;
+          }
+          if(questionMarkFound && !char.Equals(' ', c)){
+            isQuestion = false;
+          }
+          if (char.IsLower(c)){
+            allCaps = false;
+          }else if (char.IsUpper(c)){
+            capExists = true;
+          }
+          if (char.Equals(c, '?')){
+            questionMarkFound = true;
+            isQuestion = true;
+          }
+
+        }
+        if (noAlphaNum == true && isQuestion == false) {
+                   return "Fine. Be that way!";
+               } else if (allCaps == true && capExists == true && isQuestion == true) {
+                   return "Calm down, I know what I'm doing!";
+               } else if (allCaps == true && capExists == true) {
+                   return "Whoa, chill out!";
+               } else if (isQuestion == true) {
+                   return "Sure.";
+               }
+               return "Whatever.";
+
+
+    }
+}
