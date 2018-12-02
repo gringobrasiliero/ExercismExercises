f0d8612ecd651750ed3698dd60805942df2e3bdc
Nolan Byrnes <nolanehs@gmail.com>
2018-12-02 07:44:51 -0500

    Most tests completed. need quick fixes and handle exceptions

---
+++ javascript/wordy/wordy.js
@@ -0,0 +1,49 @@
+
+const numberOperators = Object.freeze({
+  "plus":       "+",
+  "minus":      "-",
+  "multiplied": "*",
+  "divided":    "/"
+});
+
+
+const ignored = /^what$|^is$|^by$/i;
+
+
+class WordProblem {
+  constructor(question){
+    this.tokens = question
+    .replace(/[,.?!]/g, "")
+    .split(" ")
+    .filter(word => !ignored.test(word))
+  }
+
+  answer() {
+    let numbers = []
+    let operators = []
+    let operWords = ["plus", "minus", "multiplied", "divided"]
+    for (var i = 0; i < this.tokens.length; i++ ){
+      var isnum = /^\d+$/.test(this.tokens[i]);
+      if (isnum){
+        numbers.push(this.tokens[i])
+      }else if(operWords.includes(this.tokens[i])){
+        operators.push(numberOperators[this.tokens[i]])
+      }
+
+    }
+
+    var result = numbers.reduce(function(arr, v, i) {
+        return arr.concat(v, operators[i]);
+    }, []);
+
+    result.pop()
+    result = result.join("")
+    return eval(result)
+  }
+}
+
+
+
+class ArgumentError extends Error {}
+
+module.exports = { WordProblem, ArgumentError }
